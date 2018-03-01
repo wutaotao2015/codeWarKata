78ee8958625b85aab6520094aa994e1736b7b07c
wutaotao2018 <wttwtt2010@163.com>
2018-03-01 13:49:18 +0800

    caesarTwoTest2

--- src/main/java/CaesarTwoTest.java
+++ src/main/java/CaesarTwoTest.java
@@ -21,13 +21,26 @@ public class CaesarTwoTest {
         List<String> v = Arrays.asList("opP DBQUBJ", "O! nz Dbqu", "bjo! pvs g", "fbsgvm usj", "q jt epof;");
         assertEquals(v, CaesarTwo.encodeStr(u, 1));
     }
+    @Test
+    public void test3() {
+        String u = "O CAPTAIN! my Captain! our fearful trip is done;";
+        List<String> v = Arrays.asList("opP DBQUBJ", "O! nz Dbqu", "bjo! pvs g", "fbsgvm usj", "q jt epof;");
+        assertEquals(u, CaesarTwo.decode(v));
+    }
+
+    @Test
+    public void test4() {
+        String u = "...allen cold and dead.[ ]";
+        List<String> v = Arrays.asList(".....b", "mmfo d", "pme bo", "e efbe", ".[ ]");
+        assertEquals(v, CaesarTwo.encodeStr(u, 1));
+    }
+
 }
 
 class CaesarTwo {
 
     public static List<String> encodeStr(String s, int shift) {
         // your code
-        s = s.trim();
         char firstLetter = Character.toLowerCase(s.charAt(0));
         char secondLetter = rotate(firstLetter, shift);
         s = firstLetter + "" + secondLetter + s;
@@ -78,14 +91,21 @@ class CaesarTwo {
     private static char rotate(char c, int shift) {
 
         if (c >= 'a' && c <= 'z') {
+
             if (c + shift > 'z') {
                 return (char) (c + shift - 'z' + 'a' - 1);
+            } else if (c + shift < 'a') {
+                return (char) (c + shift + 'z' - 'a' + 1);
             } else {
                 return (char) (c + shift);
             }
+
         } else if (c >= 'A' && c <= 'Z') {
+
             if (c + shift > 'Z') {
                 return (char) (c + shift - 'Z' + 'A' - 1);
+            } else if (c + shift < 'A') {
+                return (char) (c + shift + 'Z' - 'A' + 1);
             } else {
                 return (char) (c + shift);
             }
@@ -96,14 +116,42 @@ class CaesarTwo {
 
     public static String decode(List<String> s) {
         // your code
-        return "";
+        StringBuffer sb = new StringBuffer();
+        for (String s1 : s) {
+            sb.append(s1);
+        }
+        String stringToBeDecoded = sb.toString();
+        char prefix0 = stringToBeDecoded.charAt(0);
+        char prefix1 = stringToBeDecoded.charAt(1);
+        int shift = prefix1 - prefix0;
+        if (shift < 0) {
+            if (Character.isLowerCase(prefix1)) {
+                shift = prefix1 - 'a' + 'z' - prefix0 + 1;
+            } else {
+                shift = prefix1 - 'A' + 'Z' - prefix0 + 1;
+            }
+        }
+        stringToBeDecoded = stringToBeDecoded.substring(2,stringToBeDecoded.length());
+        StringBuffer res = new StringBuffer();
+        for (char c : stringToBeDecoded.toCharArray()) {
+            res.append(rotate(c,-shift));
+        }
+        return res.toString();
     }
 
-//    public static void main(String[] args) {
-//        System.out.println(rotate('z',2));
-//        System.out.println((int)'z');
-//        System.out.println((int)'Z');
-//        System.out.println(68 / 5);
-//        System.out.println(Math.floor(68 / 5));
-//    }
+    public static void main(String[] args) {
+        System.out.println(rotate(' ', 2));
+        System.out.println((int) 'z');
+        System.out.println((int) 'Z');
+        System.out.println(68 / 5);
+        System.out.println(Math.floor(68 / 5));
+        System.out.println('b' - 'a' + 'z' - 'x' + 1);
+        int i = 8;
+        System.out.println(-i);
+        char c = 'a';
+        int shift = -2;
+        if (c + shift < 'a') {
+            System.out.println((char) ('z' - 'a' + (c + shift) + 1));
+        }
+    }
 }
